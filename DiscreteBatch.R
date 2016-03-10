26f5fd3d6d5f9699ce455d6916b54e0d18ac2658
Todd Pierson <twpierson@Todds-MacBook-Pro.local>
2016-03-10 07:52:53 -0500

    I have this working in a bare-bones sort of way. I'm going to try to come back later and get a better understanding of what I'm doing, and I'm going to incorporate real data that I've found over the last week.

--- DiscreteBatch.R
+++ DiscreteBatch.R
@@ -1,36 +1,45 @@
 #You can use code you wrote for the correlation exercise here.
 source("DiscreteFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+tree <- read.tree("Eurycea_Tree")
+
+discrete.1<-round(runif(length(tree$tip.label)))
+discrete.2<-round(runif(length(tree$tip.label)))
+discrete.data <- data.frame(discrete.1,discrete.2)
+rownames(discrete.data) <- tree$tip.label
 
 cleaned.discrete <- CleanData(tree, discrete.data)
 
-VisualizeData(tree, cleaned.discrete)
+VisualizeData(cleaned.discrete)
 
 #First, let's use parsimony to look at ancestral states
-cleaned.discrete.phyDat <- phyDat(cleaned.discrete, type="______________") #phyDat is a data format used by phangorn
+cleaned.discrete.phyDat <- phyDat(discrete.1, type="USER",levels=c(0,1)) #phyDat is a data format used by phangorn
 anc.p <- ancestral.pars(tree, cleaned.discrete.phyDat)
 plotAnc(tree, anc.p, 1)
 
 #Do you see any uncertainty? What does that meean for parsimony?
+# There is uncertainty at the nodes. It means that there are multiple, equally 'likely'
+# (i.e., smallest number of changes) ways to reconstruct these states.
 
 #now plot the likelihood reconstruction
 anc.ml <- ancestral.pml(pml(tree, cleaned.discrete.phyDat), type="ml")
 plotAnc(tree, anc.ml, 1)
 
 #How does this differ from parsimony? 
+# The nodal estimates changed, and they're no longer 50/50 when uncertainty exists.
 #Why does it differ from parsimony?
+# The model is different; it incorporates rates of change along branchs of varying lengths.
 #What does uncertainty mean?
+# It represents the probability of those states.
 
 #How many changes are there in your trait under parsimony? 
-parsimony.score <- ____some_function_____(tree, cleanded.discrete.phyDat)
+parsimony.score <- parsimony(tree, cleaned.discrete.phyDat)
 print(parsimony.score)
 
 #Can you estimate the number of changes under a likelihood-based model? 
 
 #Well, we could look at branches where the reconstructed state changed from one end to the other. But that's not really a great approach: at best, it will underestimate the number of changes (we could have a change on a branch, then a change back, for example). A better approach is to use stochastic character mapping.
 
-estimated.histories <- make.simmap(tree, cleaned.discrete, model="ARD", nsim=5)
+estimated.histories <- make.simmap(tree, discrete.1, model="ARD", nsim=5)
 
 #always look to see if it seems reasonable
 plotSimmap(estimated.histories)
--- DiscreteFunctions.R
+++ DiscreteFunctions.R
@@ -7,10 +7,17 @@ library(phangorn)
 #You can use code you wrote for the correlation exercise here.
 
 
-VisualizeData <- function(phy, data) {
+VisualizeData <- function(data) {
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
-}
+  pdf("Visualize.pdf")
+  par(mfrow=c(1,3))
+  plot(data[[1]])
+  hist(data[[2]][,1])
+  hist(data[[2]][,2])
+  dev.off()
+  }
 
 CleanData <- function(phy, data) {
 	#treedata() in Geiger is probably my favorite function in R.
+  treedata(phy,data)
 }
